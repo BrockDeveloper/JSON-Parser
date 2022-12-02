11db5c631d02cc9e85f421b709853df011c9b27c
DamianoPellegrini <damiano1.pellegrini@gmail.com>
2022-12-02 21:52:46 +0100

    feat: jsonaccess predicate
    Co-authored-by: Andrea Broccoletti <andrea.broccoletti@brockdev.it>

--- json.pl
+++ json.pl
@@ -32,17 +32,54 @@ jpair(Name: Value, (Key, Out)) :-
 
 jobject({}, jsonobj([])) :- !.
 jobject({X}, jsonobj([Out])) :-
-    jpair(X, Out), !.
+    jpair(X, Out),
+    !.
 jobject({X}, jsonobj([Out|Outs])) :-
     X =.. [',', X1, X2],
     jpair(X1, Out),
     jobject({X2}, jsonobj(Outs)).
 
-json_parse(X, Out) :-
+jsonparse(X, Out) :-
     string(X),
+    var(Out),
     atom_string(Atom, X),
-    json_parse(Atom, Out).
-json_parse(X, Out) :- 
+    jsonparse(Atom, Out).
+
+jsonparse(X, Out) :- 
     atom(X),
+    var(Out),
     term_to_atom(Term, X),
-    jvalue(Term, Out), !.
+    jvalue(Term, Out),
+    !.
+
+jsonparse(Out, X) :-
+    var(Out),
+    nonvar(X),
+    jvalue(Term, X),
+    term_to_atom(Term, Out),
+    !.
+
+jsonaccess(jsonobj(T), [], jsonobj(T)) :- !.
+jsonaccess(O, Key, R) :- 
+    string(Key),
+    jsonaccess(O,[Key],R).
+jsonaccess(jsonobj([(Key, Value) | _]), [Key], Value) :- !.
+jsonaccess(jsonobj([(Key, Value) | _]), [Key | Keys], Out) :-
+    jsonaccess(Value, Keys, Out),
+    !.
+jsonaccess(jsonobj([_ | Ps]), [Key | Keys], Out) :-
+    jsonaccess(jsonobj(Ps), [Key | Keys], Out),
+    !.
+
+jsonaccess(jsonarray([Value | _]), [0], Value) :- !.
+jsonaccess(O, N, R) :-
+    number(N),
+    jsonaccess(O, [N], R).
+jsonaccess(jsonarray([Value | _]), [0 | Idx], Out) :-
+    jsonaccess(Value, Idx, Out),
+    !.
+jsonaccess(jsonarray([_ | Vs]), [Id | Idx], Out) :-
+    Id > 0,
+    NxId is Id - 1,
+    jsonaccess(jsonarray(Vs), [NxId | Idx], Out),
+    !.
